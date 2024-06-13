b0c18277da349f44e914cddfba99cf3c138c534e
Abhinav <125545001+abhinav7677@users.noreply.github.com>
2024-06-13 20:42:44 +0530

    Time: 1 ms (100.00%), Space: 43.9 MB (65.16%) - LeetHub

---
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
@@ -0,0 +1,46 @@
+class Solution {
+    int length;
+    int[] order;
+    public int minMovesToSeat(int[] seats, int[] students) {
+        length = seats.length;
+        order = new int[101];
+        for (int i = 0; i < length; i++) {
+            order[seats[i]]--;
+            order[students[i]]++;
+        }
+        // each EmptySeat is stored as negative value (ex: 2 seats at position 2 are empty the value order[1] = -2)
+        // each NoSeatStudent is stored as positive value (ex: 2 no corrected seat students {3, 3}, the value order[3] = 2)
+        int emptySeat = nextEmptySeat(seats, 0);
+        int student = nextStudent(students, 0);
+        int count = 0;
+        // Iterate through order to get the gaps between no corrected seat student and the empty seats
+        while (emptySeat < 101) {
+            count += Math.abs(emptySeat - student);
+            if (++order[emptySeat] == 0) {
+                emptySeat = nextEmptySeat(seats, emptySeat);
+            }
+            if (--order[student] == 0) {
+                student = nextStudent(students, student);
+            }
+        }
+        return count;
+    }
+    private int nextEmptySeat(int[] seats, int current) {
+        int i = current;
+        for (; i < 101; i++) {
+            if (order[i] < 0) {
+                return i;
+            }
+        }
+        return i;
+    }
+    private int nextStudent(int[] students, int current) {
+        int i = current;
+        for (; i < 101; i++) {
+            if (order[i] > 0) {
+                return i;
+            }
+        }
+        return i;
+    }
+}
