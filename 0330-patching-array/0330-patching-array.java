c5d7be80ed2692d3b8ff83946dfc424ef7701bc8
Abhinav <125545001+abhinav7677@users.noreply.github.com>
2024-06-16 21:28:21 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (31.70%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        int patch=0,i=0;
+        long maxReach=0;
+        while(maxReach<n)
+        {
+            if(i<nums.length && nums[i]<=(maxReach+1))
+            {
+                maxReach+=nums[i];
+                i++;
+            }
+            else
+            {
+                maxReach+=(maxReach+1);
+                patch++;
+            }
+        }
+        return patch;
+    }
+}
