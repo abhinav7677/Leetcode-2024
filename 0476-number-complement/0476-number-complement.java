ef3bc8ee8fa70bbc41c7d4750be7f580a47e5885
Abhinav <125545001+abhinav7677@users.noreply.github.com>
2024-08-22 19:28:35 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (27.99%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findComplement(int num) {
+        int len = (int )(Math.log(num)/Math.log(2))+ 1 ;
+        int mask = (1<<len)-1;
+        int ans = mask ^ num;
+        return ans;
+    }
+}
